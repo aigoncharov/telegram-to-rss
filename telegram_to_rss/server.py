2ee6321aba5c826e55e8eacbdad2e295930b6c7a
aigoncharov <andrey@goncharov.page>
2024-07-05 12:35:45 +0100

    Fix logging

--- telegram_to_rss/server.py
+++ telegram_to_rss/server.py
@@ -90,13 +90,13 @@ async def cleanup():
 
 @app.route("/")
 async def root():
-    logging.debug("GET /root", bool(client.qr_code_url))
+    logging.debug("GET /root %s", bool(client.qr_code_url))
 
     if client.qr_code_url is not None:
         qr_code_image = get_qr_code_image(client.qr_code_url)
         return await render_template("qr_code.html", qr_code=qr_code_image)
 
     feeds = await Feed.all()
-    logging.debug("GET /root -> feeds", len(feeds))
+    logging.debug("GET /root -> feeds %s", len(feeds))
 
     return await render_template("feeds.html", user=client.user, feeds=feeds)
